83fba5882a62926f0d9f9f59fb8c4daf8a768df5
kandeel <youssifk000@gmail.com>
2025-03-03 10:30:10 +0200

    adding ctf

--- scripts/blog.js
+++ scripts/blog.js
@@ -1,31 +1,30 @@
-
 async function loadBlogPosts() {
-    try {
-        const response = await fetch('../JSON/blog-posts.json');
-        const data = await response.json();
-        
-        const blogGrid = document.getElementById('blog-grid');
-        
-        // Slice the last 6 posts
-        const latestPosts = data.posts.slice(-6);
-        
-        // Build the HTML string for the blog posts
-        let blogPostsHTML = '';
-        latestPosts.forEach(post => {
-            blogPostsHTML += createBlogCard(post);
-        });
-        
-        blogGrid.innerHTML = blogPostsHTML;
-    } catch (error) {
-        console.error('Error loading blog posts:', error);
-    }
+  try {
+    const response = await fetch("./JSON/blog-posts.json");
+    const data = await response.json();
+
+    const blogGrid = document.getElementById("blog-grid");
+
+    // Slice the last 6 posts
+    const latestPosts = data.posts.slice(-6);
+
+    // Build the HTML string for the blog posts
+    let blogPostsHTML = "";
+    latestPosts.forEach((post) => {
+      blogPostsHTML += createBlogCard(post);
+    });
+
+    blogGrid.innerHTML = blogPostsHTML;
+  } catch (error) {
+    console.error("Error loading blog posts:", error);
+  }
 }
 function createBlogCard(post) {
-    const tagsHtml = post.tags.map(tag => 
-        `<span class="tag">${tag}</span>`
-    ).join('');
+  const tagsHtml = post.tags
+    .map((tag) => `<span class="tag">${tag}</span>`)
+    .join("");
 
-    return `
+  return `
         <div class="blog-card">
             <h2>${post.title}</h2>
             <div class="date">📅${post.date}</div>
@@ -38,4 +37,4 @@ function createBlogCard(post) {
     `;
 }
 
-document.addEventListener('DOMContentLoaded', loadBlogPosts);
+document.addEventListener("DOMContentLoaded", loadBlogPosts);
